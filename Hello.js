78f847a2fbf5a697490284ef05ed7c27d3719cb4
Not Real <112511577+UpperDemon@users.noreply.github.com>
2023-07-25 14:14:15 +0530

    Update Hello.js

--- Hello.js
+++ Hello.js
@@ -1,27 +1,24 @@
 function print(){
+        clutter=""
+        array.forEach(function(elem, index){
+            clutter+=`<div class="card">
+            <div class="pic">
+                <img src=${elem.pic} alt="">
+            </div>
+            <h2>${elem.name}</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore est tenetur sequi exercitationem nihil explicabo.</p>
+            <h4>${elem.status}</h4>
+            <button id="${index}" >Add friend</button>
+        </div>`
+        
     
-var clutter = ""
-arr.forEach(function(elem, index){
-    clutter+=`<div class="card">
-    <div class="pic">
-        <img src="${elem.pic}" alt="">
-    </div>
-    <h2>${elem.name}</h2>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-    <h4 >${elem.status}</h4>
-    <button id="${index}">Add Friend</button>
-</div>`
-});
-
-
-document.querySelector("#main").innerHTML=clutter
-
- };
-
- print();
-
-document.querySelector("#main").addEventListener("click", function(dets){
-    arr[dets.target.id].status="Friend Request Sent"
+        })
+        document.querySelector("#main").innerHTML=clutter
+    }
     print();
-    
-})
+
+    document.querySelector("#main").addEventListener("click",function(dets){
+        arr[dets.target.id].status = "friend request sent";
+        print();
+
+    })
