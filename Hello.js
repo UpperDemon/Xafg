a128f85b6c257670347e127e1c369b7ed82c4b31
Not Real <112511577+UpperDemon@users.noreply.github.com>
2023-07-25 12:05:04 +0530

    Update Hello.js

--- Hello.js
+++ Hello.js
@@ -1,51 +1,43 @@
-arr = [
-    
+var array =[
     {
-        name: "Person 1",
-        status: "Stranger",
-        pic: "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80"
+    name:"Kajal sen",
+    status:"Strangers",
+    pic:"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1vZGVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
     },
     {
-        name: "Person 2",
-        status: "Stranger",
-        pic: "https://images.unsplash.com/photo-1525186402429-b4ff38bedec6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+        name : "Ruchi",
+        status : "Strangers",
+        pic:"https://images.unsplash.com/photo-1564485377539-4af72d1f6a2f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1vZGVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
     },
     {
-        name: "Person 3",
-        status: "Stranger",
-        pic: "https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+        name : "Harsh Vardhan",
+        status : "Strangers",
+        pic : "https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1vZGVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
 
-    }
+    },
 ];
- function print(){
+
+    function print(){
+        clutter=""
+        array.forEach(function(elem, index){
+            clutter+=`<div class="card">
+            <div class="pic">
+                <img src=${elem.pic} alt="">
+            </div>
+            <h2>${elem.name}</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore est tenetur sequi exercitationem nihil explicabo.</p>
+            <h4>${elem.status}</h4>
+            <button id="${index}" >Add friend</button>
+        </div>`
+        
     
-var clutter = ""
-arr.forEach(function(elem, index){
-    clutter+=`<div class="box">
-    <div class="img">
-        <img src="${elem.pic}" alt="">
-    </div>
-    <h3><i>${elem.name}</i></h3>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-    <h4 id="${elem.status.split(" ").join("")}">${elem.status}</h4>
-    <button id="${index}">${elem.status === "Stranger" ? "Add Friend" : "Remove Friend"}</button>
-</div>`
-});
-
-
-document.querySelector("#main").innerHTML=clutter
-
- };
-
- print();
-
-document.querySelector("#main").addEventListener("click", function(dets){
-    arr[dets.target.id].status="Friend Request Sent"
-    setTimeout(function(){
-        arr[dets.target.id].status="Friends"
-        print();
-    
-    },2000);
+        })
+        document.querySelector("#main").innerHTML=clutter
+    }
     print();
-    
-})
+
+    document.querySelector("#main").addEventListener("click",function(dets){
+        arr[dets.target.id].status = "friend request sent";
+        print();
+
+    })
